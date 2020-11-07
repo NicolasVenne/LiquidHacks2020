e3c19283d6b7f624e7bbc6c02a281b0d476823d3
Acul132 <jrenelli1@laurentian.ca>
2020-11-07 17:12:34 -0500

    Changed backend to user express without firebase functions

--- server/functions/database/database.js
+++ server/functions/database/database.js
@@ -5,11 +5,28 @@ const admin = require("firebase-admin");
 // 2. Click Generate New Private Key, then confirm by clicking Generate Key.
 // 3. Store the JSON file in the `functions/resources/` directory
 
-var serviceAccount = require("../resources/scrim-of-legends-firebase-adminsdk-4bf1b-cef96f1cb0.json");
+var serviceAccount = null;
+
+if (process.env.NODE_ENV === 'production'){
+    console.log("PRODUCTION!!!")
+    serviceAccount = {
+        "type": "service_account",
+        "project_id": "scrim-of-legends",
+        "private_key_id": process.env.private_key_id,
+        "private_key": process.env.private_key.replace(/\\n/g, '\n'),
+        "client_email": process.env.client_email,
+        "client_id": process.env.client_id,
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-4bf1b%40scrim-of-legends.iam.gserviceaccount.com"
+      }
+} else {
+    serviceAccount = require("../resources/scrim-of-legends-firebase-adminsdk-4bf1b-cff0a4ee65.json");
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
+  credential: admin.credential.cert(serviceAccount)
 });
 
-module.exports = admin;
+module.exports = admin.firestore();
